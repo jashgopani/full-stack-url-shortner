4c6d9cb6346018ad6aad5ea02fb7fe777cf8e86d
jashgopani <jbgopani@ncsu.edu>
2024-11-10 11:37:11 -0500

    Endpoints added for creating and fetching slugs with CORS enabled

--- Backend/src/main/java/com/jashgopani/urlshortner/slug/model/Slug.java
+++ Backend/src/main/java/com/jashgopani/urlshortner/slug/model/Slug.java
@@ -4,30 +4,32 @@ package com.jashgopani.urlshortner.slug.model;
  * Model for the slug resource
  */
 public class Slug {
-    private String slug;
+    private String id;
     private String url;
 
-    public Slug() {
-    }
-
-    public Slug(String slug, String url) {
-        this.slug = slug;
+    public Slug(String id, String url) {
+        this.id = id;
         this.url = url;
     }
 
-    public String getSlug() {
-        return slug;
-    }
-
-    public void setSlug(String slug) {
-        this.slug = slug;
+    public String getId() {
+        return id;
     }
 
     public String getUrl() {
         return url;
     }
 
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public void setUrl(String url) {
         this.url = url;
+    }
+
+    @Override
+    public String toString() {
+        return "Slug [id=" + id + ", url=" + url + "]";
     }
 }
