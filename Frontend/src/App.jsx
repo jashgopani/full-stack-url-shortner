32a88652b1837b936c0e66a0b32808b18e14838a
jashgopani <jbgopani@ncsu.edu>
2024-11-10 11:53:07 -0500

    Setup React project

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className='container'>
+        <h1>URL Shortner</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
